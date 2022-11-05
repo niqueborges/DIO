c94b7a7800c553b73569b2362583f692e8b25eb2
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-11-05 13:53:02 -0300

    up

--- primeiros-passos-javascript/01 - variaveis e operadores/resolucao-desafio.js
+++ primeiros-passos-javascript/01 - variaveis e operadores/resolucao-desafio.js
@@ -1,7 +1,8 @@
 
 const precoCombustivel = 5.79;
-const kmPorLitros = 12
-const distanciaEmKm =
+const kmPorLitros = 12;
+const distanciaEmKm = 100;
 
-
-console.log
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+const valorGasto = litrosConsumidos = precoCombustivel;
+console.log(valorGasto.toFixed(2));
