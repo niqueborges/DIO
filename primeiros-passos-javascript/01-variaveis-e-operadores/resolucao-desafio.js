c1c7b64e01d2616738b90e36420d7ec95b5e9d8e
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-11-06 12:33:47 -0300

    up

--- primeiros-passos-javascript/01-variaveis-e-operadores/resolucao-desafio.js
+++ primeiros-passos-javascript/01-variaveis-e-operadores/resolucao-desafio.js
@@ -1,4 +1,14 @@
+/*
+Faça um programa para calcular o valor de uma viagem.
 
+Você terá 3 variáveis. Sendo elas:
+1 - Preço do combustível;
+2 - Gasto médio de combustível do carro por KM;
+3 - Distânica em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem.
+
+*/
 const precoCombustivel = 5.79;
 const kmPorLitros = 12;
 const distanciaEmKm = 100;
