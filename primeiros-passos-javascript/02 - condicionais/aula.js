6bddf9f28a6bbddc51bf53a2dfc4e2008ccb196a
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-11-22 18:12:05 -0300

    up

--- primeiros-passos-javascript/02 - condicionais/aula.js
+++ primeiros-passos-javascript/02 - condicionais/aula.js
@@ -5,5 +5,5 @@ const ehNumeroPar = (numero % 2) === 0;
 if (ehNumeroPar) {
     console.log('O número é par');
 } else {
-    console.log('O número é impar');
+    console.log('O número é ímpar');
 }
