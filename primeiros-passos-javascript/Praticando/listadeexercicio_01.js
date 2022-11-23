527f01fc978a1e2088a8d5e55b17d44e44d90da0
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-11-23 14:06:00 -0300

    up

--- primeiros-passos-javascript/Praticando/listadeexercicio_01.js
+++ primeiros-passos-javascript/Praticando/listadeexercicio_01.js
@@ -13,3 +13,11 @@ const nota2 = 3;
 const nota3 = 7;
 const media = (nota1 + nota2 + nota3) / 3;
 console.log(media)
+
+if (media < 5) {
+    console.log('Reprovação');
+} else if (media >= 5 && media <= 7) {
+    console.log('Recuperação');
+} else {
+    console.log('Passou de semestre');
+}
