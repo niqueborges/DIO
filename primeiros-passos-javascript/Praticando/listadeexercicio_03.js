ff11855fbebe314d80b745d86bb557af2710dd7f
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-11-23 16:00:27 -0300

    up

--- primeiros-passos-javascript/Praticando/listadeexercicio_03.js
+++ primeiros-passos-javascript/Praticando/listadeexercicio_03.js
@@ -3,11 +3,24 @@
     Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado.
 
     Código Condição de pagamento:
-    - À vista Débito, recebe 10% de desconto;
-    - À vista no dinheiro ou PIX, recebe 15% de desconto;
-    - Em duas vezes, preço normal de etiqueta sem juros;
-    - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+    1- À vista Débito, recebe 10% de desconto;
+    2- À vista no dinheiro ou PIX, recebe 15% de desconto;
+    3- Em duas vezes, preço normal de etiqueta sem juros;
+    4- Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
 */
+const precoEtiqueta = 100;
+const formaDePagamento = 4;
+
+if (formaDePagamento === 1) {
+    console.log(precoEtiqueta - (precoEtiqueta * 0.1));
+} else if (formaDePagamento === 2) {
+    console.log(precoEtiqueta - (precoEtiqueta * 0.15));
+} else if (formaDePagamento === 3) {
+    console.log(precoEtiqueta);
+} else {
+    console.log(precoEtiqueta + (precoEtiqueta * 0.1));
+}
 
 
-    
+
+
