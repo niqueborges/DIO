8ffd74102dc3c19372dd4e1e608e3212a4333511
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-11-23 13:50:23 -0300

    new

---
+++ primeiros-passos-javascript/Praticando/listadeexercicio_01.js
@@ -0,0 +1,15 @@
+/*
+    1) Faça um algoritmo que dado as 3 notas tiradas por um aluno em um semestre da faculdade, calcule e imprima a sua média e a sua classificação conforme a tabela abaixo.
+
+    Média = (nota 1 + nota 2 + nota 3) / 3;
+
+    Classificação:
+    - Média menor que 5, reprovação;
+    - Média entre 5 e 7, recuperação;
+    - Média acima de 7, passou de semestre;
+*/
+const nota1 = 5;
+const nota2 = 3;
+const nota3 = 7;
+const media = (nota1 + nota2 + nota3) / 3;
+console.log(media)
