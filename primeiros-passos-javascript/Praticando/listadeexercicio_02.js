f0c1d146c05b3286070fe2cbeabc74665a54f70f
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-11-23 15:13:40 -0300

    up

--- primeiros-passos-javascript/Praticando/listadeexercicio_02.js
+++ primeiros-passos-javascript/Praticando/listadeexercicio_02.js
@@ -13,9 +13,20 @@
     - Entre 30 e 40 Obeso;
     - Acima de 40 Obesidade grave;
 */
-const peso = 65;
-const altura = 1.70;
+const peso = 80;
+const altura = 1.72;
 const imc = peso / Math.pow(altura, 2);
 console.log(imc);
 
+if (imc < 18.5) {
+    console.log('Abaixo do peso');
+} else if (imc >= 18.5 && imc < 25) {
+    console.log('Peso normal');
+} else if (imc >= 25 && imc < 30) {
+    console.log('Acima do peso');
+} else if (imc >= 30 && imc < 40) {
+    console.log('Obeso');
+} else {
+    console.log('Obesidade grave');
+}
 
