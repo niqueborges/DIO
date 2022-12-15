da49d9d68a73d693131e334f9d3790b9dcdaf63c
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-12-15 13:56:40 -0300

    new

---
+++ primeiros-passos-javascript/07 - importaçao/main.js
@@ -0,0 +1,13 @@
+const { gets, print } = require('./funcoes-auxiliares');
+
+const quantidadeDeAlunos = gets();
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < quantidadeDeAlunos; i++) {
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado;
+    }
+}
+
+print(maiorValorEncontrado);
