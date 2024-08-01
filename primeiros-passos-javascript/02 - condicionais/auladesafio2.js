51a316821bbe5da9065d9a680bd43547f1b90b6e
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-08-01 16:45:00 -0300

    feat: aula2

--- primeiros-passos-javascript/02 - condicionais/auladesafio2.js
+++ primeiros-passos-javascript/02 - condicionais/auladesafio2.js
@@ -1,11 +1,26 @@
+/* Faça um programa para calcular o valor de uma viagem.
 
-const numero = 0;
-const numDivisivelPor5 = (numero % 5) === 0;
+Você terá 5 variáveis. Sendo elas:
+ 1 - Preço do etanol;
+ 2 - Preço do gasolina;
+ 3 - O tipo de combustível que está no seu carro;
+ 4 - Gasto médio de combustível do carro por KM;
+ 5 - Distância em KM da viagem;
 
-if(numero === 0) {
-    console.log('O número é inválido');
-} else if (numDivisivelPor5) {
-    console.log('Sim, é divisível por 5');
+Imprima no console o valor que será gasto para realizar esta viagem. */
+
+const precoEtanol = 3.96;
+const precoGasolina = 5.79;
+const tipoCombustivel = 'etanol';
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+
+if (tipoCombustivel === 'etanol') {
+  const valorGasto = litrosConsumidos * precoEtanol;
+  console.log(`O valor gasto para realizar a viagem é de R$ ${valorGasto.toFixed(2)}`);
 } else {
-    console.log('Não, não é divisível por 5');
+  const valorGasto = litrosConsumidos * precoGasolina;
+  console.log(`O valor gasto para realizar a viagem é de R$ ${valorGasto.toFixed(2)}`);
 }
