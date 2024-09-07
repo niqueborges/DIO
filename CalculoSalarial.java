d3b2ff735c682a6effe46a160a759d4f6fa28d4e
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-09-07 17:17:16 -0300

    :sparkles: feat: Calculo de salario

--- CalculoSalarial.java
+++ CalculoSalarial.java
@@ -1,5 +1,4 @@
-// Para ler o número de horas trabalhadas e o valor do salário 
-// por hora de um funcionário e calcular o salário total do funcionário.
+// Faça um programa que calcule e impriima o sal´rio a ser transferido para um funcionário.
 
 // Para ler e escrever dados em Java, aqui na Dio padronizamos da seguinte forma:
 // Para ler dados, utilizamos a classe Scanner, que é uma classe do pacote java.util.
@@ -10,19 +9,24 @@ import java.util.Scanner;
 
 public class CalculoSalarial {
     public static void main(String[] args) {
-        //Lê os valores de Entrada:
-        Scanner scanner = new Scanner(System.in);
+        try ( //Lê os valores de Entrada:
+                Scanner leitorDeEntradas = new Scanner(System.in)) {
+            float ValorSalario  = leitorDeEntradas.nextFloat(); // Lê o salário do funcionário
+            float valorbeneficios = leitorDeEntradas.nextFloat(); // Lê o valor dos benefícios do funcionário
+            
+            float valorImposto;
+            if (ValorSalario >= 0 && ValorSalario <= 1100) {
+                valorImposto = 0.05f * ValorSalario; // 5% do salário
+            } else if (ValorSalario >= 1101 && ValorSalario <= 2500) {
+                valorImposto = 0.10f * ValorSalario; // 10% do salário
+            } else {
+                valorImposto = 0.15f * ValorSalario; // 15% do salário
+            }
+        }
+           
+            // Calcula e imprime a Saída (com duas casas decimais):
+            float saída = ValorSalario - valorImposto + valorbeneficios;
+            System.out.printf("O salário líquido é de R$ %.2f\n", saída);
+        }
 
-        System.out.print("Enter the number of hours worked: ");
-        int hoursWorked = scanner.nextInt();
-
-        System.out.print("Enter the hourly wage: ");
-        double hourlyWage = scanner.nextDouble();
-
-        double salary = hoursWorked * hourlyWage;
-
-        System.out.println("The calculated salary is: " + salary);
-
-        scanner.close();
     }
-}
